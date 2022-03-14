44b283bd40733f9dee794a1d66a91fd2f56bcab2
Akozi Emmanuel <akoziemmanuel@gmail.com>
2022-03-14 10:40:03 +0100

    Files created

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+// Add Post
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).json({
+    text: res.body
+  });
+});
+
+// Delete Post
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://akozie:08176511920@vueexpresscluster0.whufu.mongodb.net/vue_express?retryWrites=true&w=majority'
+  );
+
+  return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
